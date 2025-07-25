53471b554d69141767d92f8f8a6846506a15e89a
Mohanad Beais <moh2n2dayman@gmail.com>
2025-07-25 20:00:22 +0300

    init app

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -2,6 +2,7 @@ import Subscription from "../database/models/subscription.model.js";
 export const createSubscription = async (req, res, next) => {
   try {
     const { title, price, currency, category, frequency } = req.body || {};
+    
 
     if (!title || !price || !currency || !category || !frequency) {
       const error = new Error(
